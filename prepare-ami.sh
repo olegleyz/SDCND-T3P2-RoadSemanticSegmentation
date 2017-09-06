b63ea81b962087d026fbdddef05a5d84f3ce5655
Oleg Leyzerov <olegleyz@gmail.com>
2017-09-06 19:50:48 +0200

    add script to download VGG and dataset to AMI

---
+++ prepare-ami.sh
@@ -0,0 +1,7 @@
+cd data
+wget https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip
+unzip vgg.zip
+rm vgg.zip
+wget http://kitti.is.tue.mpg.de/kitti/data_road.zip
+unzip data_road.zip
+rm data_road.zip
